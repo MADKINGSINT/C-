d1635905c52d5135e0a3a562d2d643c527367e51
BuildTools <unconfigured@null.spigotmc.org>
2022-08-29 11:51:39 +0300

    приветствие

---
+++ cc5/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите имя пользователя:  ");
+string username = Console.ReadLine();
+
+
+if(username.ToLower == "мария") {
+    Console.WriteLine("Здравствуйте, госпожа");
+}
+else if (username.ToLower == "дмитрий")  {
+    Console.WriteLine("Здравствуйте господин");
+
+}
+else if (username.ToLower == "илья") {
+    Console.WriteLine("Здравствуйте о великий хозяин");
+}
+else {
+    Console.Write("Привет, " );
+    Console.Write(username );
+    Console.WriteLine(" ");
+}
